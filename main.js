23affcd9bde37a96b68e0bc0f5e061aa56724fbd
theomorphic <FelixMK@yandex.ru>
2025-03-07 23:28:47 +0500

    feat: add animation

--- main.js
+++ main.js
@@ -8,15 +8,15 @@ scene.background = new THREE.Color('#F0F0F0');
 
 // 2. Add the camera
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.z = 5;
+camera.position.z = 1.5;
 
 // 3. Create and add an object
 
-const geometry = new THREE.BoxGeometry();
+const geometry = new THREE.LatheGeometry();
 const material = new THREE.MeshLambertMaterial({color: '#964B00', emissive: '#468585'});
 
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+const lathe = new THREE.Mesh(geometry, material);
+scene.add(lathe);
 
 // 4. Add Lightning
 const light = new THREE.DirectionalLight(0x9CDBA6, 10);
@@ -29,5 +29,15 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
 // 6. Animate the scene
-renderer.render(scene, camera);
+function animate(){
+	requestAnimationFrame(animate);
+
+	lathe.rotation.x += 0.01;
+	lathe.rotation.y += 0.02;
+
+	renderer.render(scene, camera);
+
+}
+
+animate()
 
